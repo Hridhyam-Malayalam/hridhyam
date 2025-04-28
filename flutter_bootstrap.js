fc8716846574fd1bac3381c61f7fe288772f07ba
Vineeth Chandran <vineethchandran5898@gmail.com>
2025-04-28 22:31:00 +0530

    Updation on 2025-04-28 22:31:00

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3941459245"
+    serviceWorkerVersion: "3760833234"
   }
 });
