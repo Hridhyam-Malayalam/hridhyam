b48993aa8f4d82e942d9cf87a5c3ea8eb73eb24c
Vineeth Chandran <vineethchandran5898@gmail.com>
2026-05-28 09:58:30 +0530

    Updation on 2026-05-28 09:58:29

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1ca6e1676854192ebaed0c452a1ad2d4",
+const RESOURCES = {"flutter_bootstrap.js": "cb8d39bdb6268e99999bd94c40bace76",
 "version.json": "2b2d9aba77770839a5a80d8bd62bfdd0",
 "index.html": "08ca76644a805d3f718ace0fe5a83d91",
 "/": "08ca76644a805d3f718ace0fe5a83d91",
-"main.dart.js": "da4443231b94491077835b8e8d1f95e4",
+"main.dart.js": "77eb41bb0189a8285790e8a02cdaf17e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "2e0e7b1b3b4ebb6f32945f6e9e2ddab3",
 "icons/Icon-192.png": "f08adae2b18de39f209b29bfd028f0cc",
